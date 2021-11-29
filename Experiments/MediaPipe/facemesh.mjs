3f8cfd0880dc60a2d1d8d7ecec41105b40962cf4
Chad Hart <chadwhart@gmail.com>
2021-11-28 21:30:01 -0500

    self-view flip & screen share option

--- Experiments/MediaPipe/facemesh.mjs
+++ Experiments/MediaPipe/facemesh.mjs
@@ -1,11 +1,17 @@
-const outputVideoElement = document.querySelector('video#output_video');
-const popOutBtn = document.querySelector('button#pop_out');
+const outputVideo = document.querySelector('video.output_video');
+const selfViewVideo = document.querySelector('video.self_view');
+const screenShareVideo = document.querySelector('video.screen_share');
+const popOutBtn = document.querySelector('button.pop_out');
+const screenShareBtn = document.querySelector('button.screen_share');
+const deviceSelect = document.querySelector('select#devices');
 
-const showVidCheckbox = document.querySelector('input#show_video');
-const fpsSpan = document.querySelector('span#fps');
+const showSelfViewVideoCheckbox = document.querySelector('input#show_video');
+const flipVideoCheckbox = document.querySelector('input#flip_video');
+const showScreenShareCheckbox = document.querySelector('input#show_screen_share');
+
 
 const FRAME_RATE = 15;
-const VIDEO_HEIGHT = 180;
+const VIDEO_HEIGHT = 360;
 const PIXEL_RATIO = 16 / 9;
 const VIDEO_WIDTH = VIDEO_HEIGHT * PIXEL_RATIO;
 
@@ -15,24 +21,52 @@ const canvasCtx = canvasElement.getContext('2d');
 
 // stats setup
 const stats = new Stats();
-document.querySelector('#stats').appendChild(stats.dom);
 stats.dom.style.position = 'relative';
-stats.dom.style.right = '600px';
+stats.dom.style.float = 'right';
+document.querySelector('#stats').appendChild(stats.dom);
 
+
+/*
+ *  MEdiaPipe Face Mesh setup
+ */
+
+const faceMesh = new FaceMesh({
+    locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+    }
+});
+faceMesh.setOptions({
+    maxNumFaces: 1,
+    refineLandmarks: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+});
+
+
+// Output display controller - used in as transform
 async function mesh(frame, controller) {
     // stats setup
     stats.begin();
 
-    canvasCtx.drawImage(frame, 0, 0, VIDEO_WIDTH, VIDEO_HEIGHT);
+    const flip = flipVideoCheckbox.checked ? -1: 1;
+
+
+    // MediaPipe doesn't take a frame, so need to write this to a canvas
+    // we could read right from the element in `new VideoFrame(canvasElement)`, but this lets us flip it too
+    canvasCtx.save();
+    canvasCtx.scale(flip, 1);
+    canvasCtx.drawImage(frame, 0, 0, canvasElement.width * flip, canvasElement.height);
+    canvasCtx.restore();
+
 
     await faceMesh.onResults(async results => {
-        canvasCtx.save();
-        canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
 
         // Show the video or a colored background
-        if (showVidCheckbox.checked)
-            canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
-        else {
+        // ToDo: I don't understand why this doesn't work
+        if(showScreenShareCheckbox.checked && screenShareStream.active){
+            canvasCtx.drawImage(screenShareVideo, 0, 0, canvasElement.width, canvasElement.height);
+        }
+        else if (!showSelfViewVideoCheckbox.checked){
             canvasCtx.fillStyle = "aqua";
             canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
         }
@@ -59,7 +93,6 @@ async function mesh(frame, controller) {
                 drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, WHITE_CONNECTOR);
             }
         }
-        canvasCtx.restore();
 
         const meshFrame = new VideoFrame(canvasElement);
         controller.enqueue(meshFrame);
@@ -70,31 +103,104 @@ async function mesh(frame, controller) {
     await faceMesh.send({image: canvasElement})
 }
 
-const faceMesh = new FaceMesh({
-    locateFile: (file) => {
-        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
-    }
-});
-faceMesh.setOptions({
-    maxNumFaces: 1,
-    refineLandmarks: true,
-    minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5
-});
-//faceMesh.onResults(onResults);
+
+outputVideo.onleavepictureinpicture = () => {
+    // Video element left Picture-In-Picture mode.
+    console.log("PIP closed");
+    popOutBtn.disabled = false;
+};
+
+// create a stream and send it to replace when its starts playing
+outputVideo.onplaying = () =>
+    popOutBtn.disabled = false;
+
+
+popOutBtn.onclick = async () => {
+    let pip = await outputVideo.requestPictureInPicture();
+    console.log(`Picture-in-Picture size: ${pip.width}x${pip.height}`);
+    window.pip = pip;
+    popOutBtn.disabled = true;
+};
+
 
 /*
-const camera = new Camera(videoElement, {
-    onFrame: async () => {
-        await faceMesh.send({image: videoElement});
-    },
-    // width: 1280,
-    // height: 720
-});
-camera.start();
-*/
+ * Video player controls
+ */
+if ('mediaSession' in navigator) {
 
-const deviceSelect = document.querySelector('select#devices');
+    navigator.mediaSession.metadata = new MediaMetadata({
+        title: 'WebCamEyeContact',
+        artist: "Cogint Labs",
+        /*
+        album: 'Cogint Labs Greatest Hits',
+        artwork: [
+            {src: 'https://dummyimage.com/96x96', sizes: '96x96', type: 'image/png'},
+            {src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png'},
+            {src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png'},
+            {src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png'},
+            {src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png'},
+            {src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png'},
+        ]
+
+ */
+    });
+
+
+    //     https://web.dev/media-session/
+    const actionHandlers = [
+        // ['play',          () => console.log("play") ],
+        // ['pause',         () => console.log("pause") ],
+        ['previoustrack', () => console.log("previous track")],
+        ['nexttrack', () => console.log("next track")],
+        ['togglecamera', () => {
+            showSelfViewVideoCheckbox.checked = !showSelfViewVideoCheckbox.checked;
+            navigator.mediaSession.setCameraActive(showSelfViewVideoCheckbox.checked);
+        }],
+        // ['stop',          () => console.log("stop") ],
+        // ['seekbackward',  (details) => { /* ... */ }],
+        // ['seekforward',   (details) => { /* ... */ }],
+        // ['seekto',        (details) => { /* ... */ }],
+        // ['togglemicrophone', () => { /* ... */ }],
+        // ['hangup',           () => { /* ... */ }],
+    ];
+
+    for (const [action, handler] of actionHandlers) {
+        try {
+            navigator.mediaSession.setActionHandler(action, handler);
+        } catch (error) {
+            console.log(`The media session action "${action}" is not supported yet.`);
+        }
+    }
+
+}
+
+
+/*
+ * Source video capture
+ */
+
+// ToDo: Handle screenshare aspect ratio and resizing
+//  will need to do poll $('video.screen_share').srcObject.getVideoTracks()[0].getSettings()
+
+// Screenshare selector
+let screenShareStream = new MediaStream();
+screenShareBtn.onclick = async () => {
+
+    // clean-up resources
+    screenShareStream.getTracks().forEach(track=>track.stop());
+
+    const constraints = {
+        video: {
+            // height: {ideal: 720},
+            frameRate: {ideal: FRAME_RATE},
+        }
+    };
+
+    screenShareVideo.height = VIDEO_HEIGHT;
+    screenShareStream = await navigator.mediaDevices.getDisplayMedia(constraints);
+    screenShareVideo.srcObject = screenShareStream;
+};
+
 
 async function getVideo() {
 
@@ -104,31 +210,36 @@ async function getVideo() {
 
     let videoSource = videoDevices[deviceSelect.selectedIndex || 0]?.deviceId;
 
-    const stream = await navigator.mediaDevices.getUserMedia(
-        {
-            video:
-                {
-                    height: {exact: VIDEO_HEIGHT}, width: {exact: VIDEO_WIDTH}, frameRate: FRAME_RATE,
-                    deviceId: videoSource ? {exact: videoSource} : undefined
-                }
-        });
+    const constraints = {
+        video:
+            {
+                height: {ideal: VIDEO_HEIGHT},
+                width: {ideal: VIDEO_WIDTH},
+                frameRate: {ideal: FRAME_RATE},
+                deviceId: videoSource ? {exact: videoSource} : undefined
+            }
+    };
 
-    window.stream = stream;
+    const selfViewStream = await navigator.mediaDevices.getUserMedia(constraints);
+    selfViewVideo.srcObject = selfViewStream;
 
-    const settings = stream.getVideoTracks()[0].getSettings();
-    console.log(`Capture camera with device ${stream.getVideoTracks()[0].label} at ${settings.width}x${settings.height}`);
+    const settings = selfViewStream.getVideoTracks()[0].getSettings();
+    console.log(`Capture camera with device ${selfViewStream.getVideoTracks()[0].label} at ${settings.width}x${settings.height}`);
+
 
 
     // Insertable streams
-    const [track] = stream.getVideoTracks();
+    const [track] = selfViewStream.getVideoTracks();
     const processor = new MediaStreamTrackProcessor({track});
 
     const generator = new MediaStreamTrackGenerator({kind: 'video'});
-    outputVideoElement.srcObject = new MediaStream([generator]);
+    outputVideo.srcObject  = new MediaStream([generator]);
 
     await processor.readable.pipeThrough(new TransformStream({
         transform: (frame, controller) => mesh(frame, controller)
     })).pipeTo(generator.writable);
+
+    window.outputStream = selfViewStream;
 
 }
 
@@ -149,107 +260,11 @@ async function start() {
     canvasElement.height = VIDEO_HEIGHT;
     canvasElement.width = VIDEO_WIDTH;
 
-    // create a stream and send it to replace when its starts playing
-    outputVideoElement.onplaying = () =>
-        popOutBtn.disabled = false;
+    await getDevices();
 
     // Note: list of devices may change after first camera permission approval
     await getVideo();
 }
 
-popOutBtn.onclick = async () => {
-    const PIP_HEIGHT = 180;
-    let pip = await outputVideoElement.requestPictureInPicture();
-    console.log(`Picture-in-Picture size: ${pip.width}x${pip.height}`);
-    window.pip = pip;
-    popOutBtn.disabled = true;
-}
-
-outputVideoElement.onleavepictureinpicture = () => {
-    // Video element left Picture-In-Picture mode.
-    console.log("PIP closed");
-    popOutBtn.disabled = false;
-};
-
-
-if ('mediaSession' in navigator) {
-
-    navigator.mediaSession.metadata = new MediaMetadata({
-        title: 'WebCamEyeContact',
-        artist: "Cogint Labs",
-        album: 'Cogint Labs Greatest Hits',
-        artwork: [
-            {src: 'https://dummyimage.com/96x96', sizes: '96x96', type: 'image/png'},
-            {src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png'},
-            {src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png'},
-            {src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png'},
-            {src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png'},
-            {src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png'},
-        ]
-    });
-
-    /*
-    navigator.mediaSession.setActionHandler('play', ()=>console.log("play"));
-    navigator.mediaSession.setActionHandler('pause', ()=>console.log("pause"));
-    navigator.mediaSession.setActionHandler('seekbackward', ()=>console.log("seek back"));
-    navigator.mediaSession.setActionHandler('seekforward', ()=>console.log("seek forward"));
-    navigator.mediaSession.setActionHandler('seekto', ()=>console.log("seek forward"));
-    navigator.mediaSession.setActionHandler('previoustrack', ()=>console.log("previous track"));
-    navigator.mediaSession.setActionHandler('nexttrack', ()=>console.log("next track"));
-    navigator.mediaSession.setCameraActive(true);
-    navigator.mediaSession.setMicrophoneActive(true);
-
-     */
-
-    //     https://web.dev/media-session/
-    const actionHandlers = [
-        // ['play',          () => console.log("play") ],
-        // ['pause',         () => console.log("pause") ],
-        ['previoustrack', () => console.log("previous track")],
-        ['nexttrack',     () => console.log("next track")],
-        // ['stop',          () => console.log("stop") ],
-        // ['seekbackward',  (details) => { /* ... */ }],
-        // ['seekforward',   (details) => { /* ... */ }],
-        // ['seekto',        (details) => { /* ... */ }],
-        // ['togglemicrophone', () => { /* ... */ }],
-        ['togglecamera',     () => {
-            showVidCheckbox.checked = !showVidCheckbox.checked;
-            navigator.mediaSession.setCameraActive(showVidCheckbox.checked);
-        }],
-        // ['hangup',           () => { /* ... */ }],
-    ];
-
-    for (const [action, handler] of actionHandlers) {
-        try {
-            navigator.mediaSession.setActionHandler(action, handler);
-        } catch (error) {
-            console.log(`The media session action "${action}" is not supported yet.`);
-        }
-    }
-
-    /*
-    //  https://developer.mozilla.org/en-US/docs/Web/API/MediaSession#examples
-    [
-        'play',
-        'pause',
-        'seekbackward',
-        'seekforward',
-        'seekto',
-        'previoustrack',
-        'nexttrack',
-        'skipad',
-        'togglemicrophone',
-        'togglecamera',
-        // 'hangup'
-    ]
-        .forEach(action => navigator.mediaSession.setActionHandler(action, () => console.log(`mediaSession.setActionHandler event: ${action}`))
-        );
-
-     */
-
-}
-
-await getDevices();
 deviceSelect.onchange = getVideo;
 start().catch(err => console.error(err));
-//startBtn.onclick = async ()=> (await start()).catch(err=>console.error(err));
